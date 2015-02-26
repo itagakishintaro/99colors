1becb706239cf4d5adce622a2205fb250b55a8b7
itagaki <itagaki.shintaro@gmail.com>
2015-02-27 00:02:10 +0900

    Add REST API find and update

--- public/js/99colors.js
+++ public/js/99colors.js
@@ -1,32 +1,79 @@
 'use strict';
 
+var input_dom = '<div class="input"></div>';
+var color_dom = '<input type="text" class="form-control color-code" placeholder="Color code">';
+var ok_dom = '<div name="ok" class="btn btn-sm btn-default pull-right">OK</div>';
+var sample_dom = '<div class="sample"></div>';
 
 $(function() {
-	createPalette();
+    createPalette();
 
     $('.color-code').keyup(function() {
         $(this).next('.sample').css('background-color', $(this).val());
     });
 
     $('[name=ok]').click(function() {
-        var color = $(this).prevAll('input').val();
-        $(this).parent().parent().append('<div class="color" data-clipboard-text="' + color + '" data-toggle="modal" data-target="#myModal"></div>');
-        var sample = $(this).parent().next('.color');
-        sample.css('background-color', color);
-
-        sample.click(function(){
-        	var clip = new ZeroClipboard(sample);
-        });
+        var color_code = $(this).prevAll('input').val();
+        $(this).parent().parent().append('<div class="color" data-clipboard-text="' + color_code + '"></div>');
+        // $(this).parent().parent().append('<div class="color" data-clipboard-text="' + color_code + '" data-toggle="modal" data-target="#myModal"></div>');
+        setColor($(this).parent().next('.color'), color_code);
         // $(this).parent().next('.color').draggable();
+    });
+
+    $('#save').click(function() {
+        save();
+    });
+
+    $('#load').click(function() {
+        load();
     });
 });
 
-function createPalette(){
-	for(var i = 1; i <= 100; i++){
-		$('.palette').append('<div id="' + i + '" class="area"></div>');
-		$('#' + i).append('<div class="input"></div>');
-		$('#' + i + ' .input').append('<input type="text" class="form-control color-code" placeholder="Color code">');
-		$('#' + i + ' .input').append('<div class="sample"></div>');
-		$('#' + i + ' .input').append('<div name="ok" class="btn btn-sm btn-default pull-right">OK</div>');
-	}	
+function setColor(color, color_code) {
+    color.css('background-color', color_code);
+
+    color.click(function() {
+        var clip = new ZeroClipboard(color);
+        $(this).addClass('animated pulse').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $(this).removeClass('animated pulse');
+        });
+    });
+}
+
+function createPalette() {
+    for (var i = 1; i <= 99; i++) {
+        $('.palette').append('<div id="' + i + '" class="area"></div>');
+        $('#' + i).append(input_dom);
+        $('#' + i + ' .input').append(color_dom);
+        $('#' + i + ' .input').append(sample_dom);
+        $('#' + i + ' .input').append(ok_dom);
+    }
+}
+
+function save() {
+    var colors = {};
+    for (var i = 1; i <= 99; i++) {
+        var color_code = $('#' + i + ' .color-code').val();
+        colors[i] = {
+            color_code: color_code
+        };
+    }
+    console.log(JSON.stringify(colors));
+    $.post('/api/update/itagaki', colors);
+}
+
+function load() {
+    $.getJSON('/api/find/itagaki', function(json) {
+        $('.color').remove();
+        for (var i = 1; i <= 99; i++) {
+            var color_code = json[0].palette[i].color_code;
+            if (color_code === '') {
+                return;
+            }
+            $('#' + i + ' .input .color-code').val(color_code);
+            $('#' + i + ' .input .sample').css('background-color', color_code);
+            $('#' + i).append('<div class="color" data-clipboard-text="' + color_code + '"></div>');
+            setColor($('#' + i + ' .color'), color_code);
+        }
+    });
 }
--- routes.js
+++ routes.js
@@ -1,10 +1,31 @@
 'use strict';
 var configRoutes;
+var mongo = require('./mongo');
+var bodyParser = require('body-parser');
 
 configRoutes = function(app, server) {
-    app.get('/', function(request, response) {
-        response.redirect('/index.html');
+	app.use(bodyParser.urlencoded({extended: true}));
+
+    app.get('/', function(req, res) {
+        res.redirect('/index.html');
+    });
+
+    app.get('/api/find/:name', function(req, res) {
+        mongo.find('palettes', {name: req.params.name}, {}, 
+            function(list){
+                res.send(list);
+            }
+        );
+    });
+    app.post('/api/update/:name', function(req, res) {
+        var colors = req.body;
+console.dir(colors);
+        mongo.update('palettes', {name: req.params.name}, {name: req.params.name, palette: colors}, { upsert: true }, function(result) {
+            res.send(result);
+        });
     });
 }
 
-module.exports = {configRoutes: configRoutes};
+module.exports = {
+    configRoutes: configRoutes
+};
